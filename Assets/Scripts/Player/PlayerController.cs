0117122a8f6890292cf079347ba7fa924bab6a27
phuocn0302 <phuocnguyen230302@gmail.com>
2024-09-28 14:53:49 +0700

    Tweak player movement code and add simple dash mechanic

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,24 +5,64 @@ using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private TrailRenderer tr;
+
+    [Header("Movement settings")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Dash settings")]
+    [SerializeField] private float dashSpeed = 20f;
+    [SerializeField] private float dashDuration = 0.2f;
+    [SerializeField] private float dashCooldown = 0.8f;
+
+    private Vector2 direction;
+    private bool isDashing = false;
+    private bool canDash = true;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<TrailRenderer>();
     }
+
     private void Start()
     {
-        
+        tr.time = 0.3f;
     }
+
     private void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");    
+        if (isDashing) return; // No input while dashing
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");    
+        direction.Normalize();
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDash) 
+        {
+            StartCoroutine(Dash());
+        }
     }
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position +moveInput.normalized*speed*Time.deltaTime);
+        if (isDashing) return;
+        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        Vector2 dashDir = direction;
+
+        rb.velocity = new Vector2(dashDir.x * dashSpeed, dashDir.y * dashSpeed);
+        tr.emitting = true;
+
+        yield return new WaitForSeconds(dashDuration);
+        tr.emitting = false;  
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
